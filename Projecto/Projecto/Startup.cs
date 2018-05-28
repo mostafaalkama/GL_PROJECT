cded21d09b87aae0a92f090b55f7b5f6383e5ed7
mostafaalkama <mostafaa.alkamaa.1996@gmail.com>
2018-05-28 16:57:11 +0000

    add project with auth and test unit

---
+++ Projecto/Projecto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projecto.Startup))]
+namespace Projecto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
